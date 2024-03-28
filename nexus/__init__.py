7aac54882a58c14c15477346e7b864e93bfd1f58
Andreu Vallbona <avallbona@gmail.com>
2024-03-28 16:02:11 +0100

    bumped package version

--- nexus/__init__.py
+++ nexus/__init__.py
@@ -8,7 +8,7 @@ from django.utils.module_loading import autodiscover_modules
 from nexus.modules import NexusModule
 from nexus.sites import NexusSite, site
 
-__version__ = "2.1.2"
+__version__ = "3.0.0"
 VERSION = __version__
 __all__ = ("autodiscover", "NexusSite", "NexusModule", "site")
 
